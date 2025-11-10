46b1145fc6d0e7a42925c9b41e4eefac401f444e
Ishika Verma <ishikaverma2317@gmail.com>
2025-11-10 16:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
-//complete this code
-class Rectangle {}
+// Rectangle class
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+// Square class inheriting from Rectangle
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // call parent constructor with equal width and height
+  }
+
+  // Method to calculate perimeter
+  getPerimeter() {
+    return 4 * this._width;
+  }
+}
+
+// ✅ Example usage:
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); // 5
+console.log(rectangle.height); // 10
+console.log(rectangle.getArea()); // 50
+
+const square = new Square(7);
+console.log(square.width); // 7
+console.log(square.height); // 7
+console.log(square.getArea()); // 49
+console.log(square.getPerimeter()); // 28
+
+// Make the classes accessible for testing
 window.Rectangle = Rectangle;
 window.Square = Square;
